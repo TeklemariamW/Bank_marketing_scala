7a68c18a5becd7aab1d6dddee58b761fdb956990
Teklemariam Weldehawariat <tekaliklfe@gmail.com>
2024-06-30 19:31:10 -0400

    implement increamental load

--- src/main/scala/Increamentalload.scala
+++ src/main/scala/Increamentalload.scala
@@ -1,60 +1,59 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
 
-object Increamentalload {
+object IncrementalLoad {
   def main(args: Array[String]): Unit = {
-
-    val spark: SparkSession = SparkSession.builder()
-      .master("local[*]")
-      .appName("MiniPrjScala")
+    // Initialize SparkSession
+    val spark = SparkSession.builder()
+      .appName("IncrementalLoad")
+      .config("spark.master", "local[*]")  // Adjust as per your deployment environment
       .enableHiveSupport()
       .getOrCreate()
 
-    val df = spark.read.format("jdbc")
-      .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb")
-      .option("dbtable", "bank")
-      .option("driver", "org.postgresql.Driver")
-      .option("user", "consultants")
-      .option("password", "WelcomeItc@2022").load()
+    try {
+      // Read data from PostgreSQL table
+      val df = spark.read.format("jdbc")
+        .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb")
+        .option("dbtable", "bank")
+        .option("driver", "org.postgresql.Driver")
+        .option("user", "consultants")
+        .option("password", "WelcomeItc@2022")
+        .load()
 
-    df.printSchema()
-    df.show()
+      // Print schema and sample data from PostgreSQL
+      df.printSchema()
+      df.show()
 
-    // read and show the existing_data in hive table
-    val existing_hive_data = spark.read.table("tekle.bank_marketing_scala")
-    existing_hive_data.show(5)
+      // Read existing data from Hive table
+      val existing_hive_data = spark.read.table("tekle.bank_marketing_scala")
+      existing_hive_data.show(5)
 
-    // 4. Determine the incremental data
-    /*
-   "left_anti" specifies the type of join to perform.
-    A left_anti join returns only the rows from the left DataFrame (df_upper in this case)
-    that do not have corresponding matches in the right DataFrame (existing_hive_data).
-    Essentially, it finds rows in df_upper that are not present in existing_hive_data based
-    on id.
-     */
-    val incremental_data_df = df_upper.join(existing_hive_data, Seq("id"), "left_anti")
-    print("------------------Incremental data-----------------------")
-    incremental_data_df.show()
+      // Determine incremental data using left_anti join
+      val incremental_data_df = df.join(existing_hive_data, Seq("id"), "left_anti")
+      println("------------------Incremental data-----------------------")
+      incremental_data_df.show()
 
-    //counting the number of the new records added to postgres tables
-    val new_records = incremental_data_df.count()
-    print("------------------COUNTING INCREMENT RECORDS ------------")
-    print("new records added count", new_records)
+      // Count new records added to PostgreSQL table
+      val new_records = incremental_data_df.count()
+      println("------------------COUNTING INCREMENT RECORDS ------------")
+      println(s"New records added count: $new_records")
 
-    // 5.  Adding the incremental_data DataFrame to the existing hive table
-    // Check if there are extra rows in PostgresSQL. if exist => # write & append to the Hive table
-    if (incremental_data_df.count() > 0) {
-      // Append new rows to Hive table
-      incremental_data_df.write.mode("Append").saveAsTable("tekle.bank_marketing_scala")
-
-    }
-    else{
-        print("No new records been inserted in PostgresSQL table.")
+      // Append incremental_data_df to the existing Hive table if there are new records
+      if (new_records > 0) {
+        incremental_data_df.write.mode("append").saveAsTable("tekle.bank_marketing_scala")
+        println("New records appended to Hive table.")
+      } else {
+        println("No new records appended to Hive table.")
       }
-    }
-   //Read again from hive to see the updated data
-    val updated_hive_data = spark.read.table("tekle.bank_marketing_scala")
-    val df_ordered = updated_hive_data.orderBy(col("id").desc())
-    df_ordered.show(5)
 
+      // Read updated data from Hive table and display ordered by id descending
+      val updated_hive_data = spark.read.table("tekle.bank_marketing_scala")
+      val df_ordered = updated_hive_data.orderBy(col("id").desc())
+      println("Updated Hive table:")
+      df_ordered.show(5)
+    } finally {
+      // Stop SparkSession at the end
+      spark.stop()
+    }
+  }
 }
